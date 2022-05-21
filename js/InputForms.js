95cc0e11e816a9efd2115d6b96549e70a2c32202
yash-agrwl <agarwalyash7012@gmail.com>
2022-05-21 23:35:21 +0530

    added UC2: Implement javascript to validate Email and show error message if invalid

--- js/InputForms.js
+++ js/InputForms.js
@@ -8,3 +8,14 @@ name.addEventListener('input', function () {
     else
         nameError.textContent = "Name is Incorrect";
 });
+
+// Email Validation.
+let email = document.querySelector("#email");
+let emailError = document.querySelector(".Email-error");
+email.addEventListener('input', function () {
+    let emailRegex = RegExp('^[a-z0-9]+([-+_.][a-z0-9]+)?[@][a-z0-9]+[.][a-z0-9]{2,3}([.][a-z]{2})?$');
+    if (emailRegex.test(email.value))
+        emailError.textContent = "";
+    else
+        emailError.textContent = "Email is Invalid";
+});
